ba387ea43a32c738bdf658cf2078a0bf6b491e75
EliYakov <eliko0807@gmail.com>
2022-12-12 15:39:12 +0200

    CreateCardDone

--- src/components/mycards.jsx
+++ src/components/mycards.jsx
@@ -1,5 +1,30 @@
+import { useMyCards } from "./hooks/useMyCards";
+import PageHeader from "./common/pageheader";
+import { Link } from "react-router-dom";
+import Card from "./card";
 const MyCards = () => {
-  return <p>my cards</p>;
+  const cards = useMyCards();
+
+  return (
+    <>
+      <PageHeader
+        title="My Cards"
+        Description="Your cards is showing below in the list"
+      />
+
+      <div className="row">
+        <Link to="/create-cards">Create New Card</Link>
+      </div>
+
+      <div className="row">
+        {!cards.length ? (
+          <p>No Cards..</p>
+        ) : (
+          cards.map((card) => <Card key={card._id} card={card} />)
+        )}
+      </div>
+    </>
+  );
 };
 
 export default MyCards;
